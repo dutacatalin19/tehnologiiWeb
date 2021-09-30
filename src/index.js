b1e755442bb7eda166e36f719a5161ee3bc8e813
Duta Catalin Ionut <dutacatalin19@stud.ase.ro>
2021-09-30 09:55:16 +0000

    final commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-var http = require("http");
-
-//create a server object:
-http
-  .createServer(function(req, res) {
-    res.write("Hello World!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+app.use(express.static(__dirname + '/public'));
+app.listen(8080);
